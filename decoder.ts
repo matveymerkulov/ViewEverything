9cb48fda2ec6d3b6dc0d25061d99c53d2e248d00
Matvey Merkulov <32887734+matveymerkulov@users.noreply.github.com>
2025-08-21 22:00:28 +1100

    0.4 format detecting, format parameters

--- decoder.ts
+++ decoder.ts
@@ -1,29 +1,53 @@
 import {electron} from "./main.js";
+import {formats} from "./formats.js";
 
-export const palette: number[][] = []
+export let currentPalette: number[][] = undefined
 
 export function decode(fileName: string) {
     const data: Uint8Array = electron.getData(fileName)
-    const width = 320
-    const height = 200
 
-    const canvas = document.createElement("canvas")
-    canvas.width = width
-    canvas.height = height
-    const ctx = canvas.getContext("2d")
+    for(const format of formats) {
+        console.log(format.name)
 
-    for(let colIndex = 0; colIndex < 256; colIndex++) {
-        const i = 64007 + colIndex * 3
-        palette[colIndex] = [data[i], data[i + 1], data[i + 2]]
-    }
+        const fileSize = format.fileSize
+        if(fileSize !== undefined && data.length !== fileSize) continue
 
-    for(let y = 0; y < height; y++) {
-        for(let x = 0; x < width; x++) {
-            const color = palette[data[x + width * y + 7]]
-            ctx.fillStyle = `rgb(${color[0] * 4}, ${color[1] * 4}, ${color[2] * 4})`
-            ctx.fillRect(x, y, 1, 1)
+        const paletteStart = format.paletteStart
+        if(paletteStart !== undefined) {
+            currentPalette = []
+            const mul = format.paletteMultiplier ?? 1
+            const indexMul = format.paletteBytesPerColor ?? 1
+            for (let colIndex = 0; colIndex < 256; colIndex++) {
+                currentPalette[colIndex] = []
+                for (let colorLayer = 0; colorLayer < 3; colorLayer++) {
+                    const i = (colorLayer + colIndex * 3) * indexMul
+                    currentPalette[colIndex][colorLayer] = data[i] * mul
+                }
+            }
+        } else if(currentPalette === undefined) {
+            console.log("no palette")
+            return
         }
-    }
 
-    return canvas
+        const width = format.width
+        const height = format.height
+        if(width === undefined || height === undefined) return
+
+        //console.log(format.name)
+
+        const canvas = document.createElement("canvas")
+        canvas.width = width
+        canvas.height = height
+        const ctx = canvas.getContext("2d")
+
+        for(let y = 0; y < height; y++) {
+            for(let x = 0; x < width; x++) {
+                const color = currentPalette[data[x + width * y + 7]]
+                ctx.fillStyle = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+                ctx.fillRect(x, y, 1, 1)
+            }
+        }
+
+        return canvas
+    }
 }
--- formats.ts
+++ formats.ts
@@ -1,10 +1,40 @@
-export let formats: Object = [
+export let formats: any = [
     {
+        name: "Binary VGA palette",
+        fileSize: 768,
+
+        paletteStart: 0,
+        paletteMultiplier: 4,
+        paletteBytesPerColor: 1,
+    }, {
+        name: "Alternate Logic Puzzles VGA palette",
+        fileSize: 1536,
+
+        paletteStart: 0,
+        paletteMultiplier: 4,
+        paletteBytesPerColor: 2,
+        colorLayersFirst: true,
+    }, {
         name: "QBasic BSAVE VGA fullscreen image",
+        type: "QBasic Binary",
         fileSize: 64007,
+
         imageStart: 7,
         width: 320,
         height: 200,
         bitsPerPixel: 8,
+    }, {
+        name: "QBasic BSAVE VGA fullscreen image with palette",
+        type: "QBasic Binary",
+        fileSize: 64775,
+
+        imageStart: 7,
+        width: 320,
+        height: 200,
+        bitsPerPixel: 8,
+
+        paletteStart: 64007,
+        paletteMultiplier: 4,
+        paletteBytesPerColor: 1,
     },
 ]
